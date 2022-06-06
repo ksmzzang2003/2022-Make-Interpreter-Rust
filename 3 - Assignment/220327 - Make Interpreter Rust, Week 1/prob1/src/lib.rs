e1592571debfad05fcf1e1111046b3a3ca1f429b
ksmzzang2003 <ksmzzang2003@naver.com>
2022-06-06 16:29:26 +0900

    Update lib.rs

--- 3 - Assignment/220327 - Make Interpreter Rust, Week 1/prob1/src/lib.rs
+++ 3 - Assignment/220327 - Make Interpreter Rust, Week 1/prob1/src/lib.rs
@@ -6,11 +6,44 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        //unimplemented!("Revive this player");
+        if self.health > 0 {return None;}
+        else if self.level < 10 {
+            return Some(Player{
+                health : 100, 
+                mana : None, 
+                level : self.level,
+        }
+        else {
+            return Some(Player{
+                health : 100, 
+                mana : Some(100), 
+                level : self.level,
+            });
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        //unimplemented!("Cast a spell of cost {}", mana_cost);
+        if self.mana.is_none() {
+            if self.health >= mana_cost {
+                self.health -= mana_cost;
+            }
+            else {
+                self.health = 0; 
+            }
+            return 0;
+        }
+        else {
+            let mp = self.mana.unwrap(); 
+            if mp < mana_cost {
+                return 0;
+            }
+            else {
+                self.mana = Some(mp - mana_cost);
+                return 2*mana_cost; 
+            }
+        }
     }
 }
 
